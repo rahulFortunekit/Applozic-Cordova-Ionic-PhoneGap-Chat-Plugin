8eadf5be3517128292b3ef10aaa5959e8dfade03
Devashish Datt Mamgain <devashish@applozic.com>
2017-04-22 20:29:02 +0530

    correction

--- www/applozic.js
+++ www/applozic.js
@@ -4,10 +4,10 @@ module.exports = {
     login: function(alUser, successCallback, errorCallback) {
     	cordova.exec(successCallback, errorCallback, "ALChatManagerCDVPlugin", "login", [JSON.stringify(alUser)]);
     },
-    launchChat: function(userId, successCallback, errorCallback) {
-    	cordova.exec(successCallback, errorCallback, "ALChatManagerCDVPlugin", "launchChat", [userId]);
+    launchChat: function(successCallback, errorCallback) {
+    	cordova.exec(successCallback, errorCallback, "ALChatManagerCDVPlugin", "launchChat", []);
     },
-    launchChatWithUserId: function(successCallback, errorCallback) {
+    launchChatWithUserId: function(userId, successCallback, errorCallback) {
     	cordova.exec(successCallback, errorCallback, "ALChatManagerCDVPlugin", "launchChatWithUserId", [userId]);
     },
     launchChatWithGroupId: function(groupId, successCallback, errorCallback) {
